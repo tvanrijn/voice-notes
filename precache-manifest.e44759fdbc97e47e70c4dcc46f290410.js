39506f93d1d962f6bf3f29574fc83c909eb048c6
tvanrijn <vanrijn.tineke@gmail.com>
2019-02-12 20:16:14 +0100

    Updates

---
+++ precache-manifest.e44759fdbc97e47e70c4dcc46f290410.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cbf3a829f0d4e48a375",
+    "url": "/tvanrijn/voice-notes/static/js/runtime~main.0cbf3a82.js"
+  },
+  {
+    "revision": "a8ca2c0883921af7ccbf",
+    "url": "/tvanrijn/voice-notes/static/js/main.a8ca2c08.chunk.js"
+  },
+  {
+    "revision": "c77fe369cfd3d9365016",
+    "url": "/tvanrijn/voice-notes/static/js/1.c77fe369.chunk.js"
+  },
+  {
+    "revision": "a8ca2c0883921af7ccbf",
+    "url": "/tvanrijn/voice-notes/static/css/main.20e97755.chunk.css"
+  },
+  {
+    "revision": "ea9bb43afab78fbd70ec999f737560a8",
+    "url": "/tvanrijn/voice-notes/index.html"
+  }
+];
