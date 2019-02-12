41d6c4fc3071381b4f9bd923bf328a506fa547f6
tvanrijn <vanrijn.tineke@gmail.com>
2019-02-12 21:10:04 +0100

    update footer and readme

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,42 +9,46 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      text: '',
-      copySuccess: ''
+      text: ''
     }
   }
 
-  onListenClick() {
-    var access_token = "Empty";
+  onListenClick = () => {
+    fetch('/api/speech-to-text/token')
+      .then((response) => {
+        return response.text();
+      }).then((token) => {
+        var stream = recognizeMicrophone({
+          access_token: token
+        });
 
-    var stream = recognizeMicrophone({
-      access_token: access_token
-    });
-    
-    var imgMicrophone = document.getElementById("mic");
-    imgMicrophone.classList.add("active");
+        var imgMicrophone = document.getElementById("mic");
+        imgMicrophone.classList.add("active");
 
-    stream.setEncoding('utf8');
-    
-    stream.on('data', function(data) {
-      var notes = this.state.text.concat(data);
-      this.setState({ text: notes});
-    }.bind(this));
-    
-    stream.on('error', function(err) {
-      console.log(err);
-    });
+        stream.setEncoding('utf8');
 
-    document.querySelector('#stop').addEventListener('click', () => {
-      stream.stop(); 
-      imgMicrophone.classList.remove("active");
-    });
+        stream.on('data', function (data) {
+          var notes = this.state.text.concat(data);
+          this.setState({ text: notes });
+        }.bind(this));
+
+        stream.on('error', function (err) {
+          console.log(err);
+        });
+
+        document.querySelector('#stop').addEventListener('click', () => {
+          stream.stop();
+          imgMicrophone.classList.remove("active");
+        });
+      }).catch((error) => {
+        console.log(error);
+      });
   }
 
   addUserInput(event) {
-      this.setState({text: event.target.value});
+    this.setState({ text: event.target.value });
   }
-  
+
   onCopyClick() {
     var textField = document.createElement('textarea')
     textField.innerText = this.state.text;
@@ -52,26 +56,24 @@ class App extends Component {
     textField.select();
     document.execCommand("copy");
     textField.remove();
-    var alertMessage = 'Copied!'
-    this.setState({ copySuccess: alertMessage });
   }
 
   onDateClick() {
     var dateToday = new Date().toLocaleString() + ': ';
     var notes = dateToday.concat(this.state.text);
-    this.setState({ text: notes});
+    this.setState({ text: notes });
   }
 
   onClearClick() {
-    this.setState({ text: ''});
+    this.setState({ text: '' });
   }
 
   render() {
     return (
       <div className="App">
-        <MicControls onListenClick={this.onListenClick.bind(this)}/>
-        <Notes showNotesText={this.state.text} addUserInput={this.addUserInput.bind(this)}/>
-        <NotesControls onDateClick={this.onDateClick.bind(this)} onCopyClick={this.onCopyClick.bind(this)} onClearClick={this.onClearClick.bind(this)}/>
+        <MicControls onListenClick={this.onListenClick.bind(this)} />
+        <Notes showNotesText={this.state.text} addUserInput={this.addUserInput.bind(this)} />
+        <NotesControls onDateClick={this.onDateClick.bind(this)} onCopyClick={this.onCopyClick.bind(this)} onClearClick={this.onClearClick.bind(this)} />
       </div>
     );
   }
